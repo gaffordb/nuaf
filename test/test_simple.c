ac91696501e92a93ca0f08acadd49b18dd433937
Ben Gafford <gaffordb@grinnell.edu>
2019-04-26 08:38:42 -0500

    added simple test

---
+++ test/test_simple.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  printf("Making allocations...\n");
+  void* p1 = malloc(512);
+  void* p2 = malloc(0x4000);
+  void* p3 = malloc(64);
+  void* p4 = malloc(5);
+  
+  free(p1);
+  free(p2);
+  free(p3);
+
+  printf("Dereferencing okay pointer...\n");
+  int x = *(int*)p4;
+  printf("Dereferencing bad pointer...\n");
+  x = *(int*)p3;
+  printf("Should have segfaulted...\n");
+
+}
